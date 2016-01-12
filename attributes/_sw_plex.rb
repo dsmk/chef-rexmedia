3134502a93b3a96b445b16ba2de2734444d31e67
David King <david@rexden.us>
2016-01-12 18:15:32 -0500

    Adding plex version number

--- attributes/_sw_plex.rb
+++ attributes/_sw_plex.rb
@@ -16,7 +16,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-default['rexden']['vagrant_version'] = '1.8.1'
-default['rexden']['virtualbox_version'] = "5.0.12"
-default['rexden']['virtualbox_rel'] = "5.0.12_104815"
+default['rexden']['plex_version'] = '0.9.12.3.1173-937aac3'
 
