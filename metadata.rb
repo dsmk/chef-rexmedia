b936dd9023d93d4440162706e019bcf6efb3441b
David King <david@rexden.us>
2016-01-13 14:34:25 -0500

    Upgrading metadata

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures rexmedia'
 long_description 'Installs/Configures rexmedia'
-version '0.1.0'
+version '0.1.1'
 
 depends "yum-epel"
 depends "rexcore"
